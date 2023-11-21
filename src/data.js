0dd911bd96223a024fcf03191059b50384ff69c4
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-21 11:23:34 -0500

    add contentful images and urls

--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 const projects = [
   {
-    title: 'birthday buddy',
-    url: 'https://react-vite-projects-1-birthday-buddy.netlify.app/',
-    image: './assets/birthday.png',
+    title: 'soccer team',
+    url: 'https://sun-n-y.github.io/soccerPlayerList/',
+    image: './assets/soccer.PNG',
   },
   {
     title: 'tours',
-    url: 'https://react-vite-projects-2-tours.netlify.app/',
-    image: './assets/tours.png',
+    url: 'https://sun-n-y.github.io/tourCards/',
+    image: './assets/mytours.png',
   },
   {
     title: 'reviews',
-    url: 'https://react-vite-projects-3-reviews.netlify.app/',
-    image: './assets/reviews.png',
+    url: 'https://sun-n-y.github.io/reviewsSolo/',
+    image: './assets/myreviews.png',
   },
   {
     title: 'questions',
-    url: 'https://react-vite-projects-4-accordion.netlify.app/',
-    image: './assets/questions.png',
+    url: 'https://sun-n-y.github.io/questions/',
+    image: './assets/myquestions.png',
   },
 ];
