e69d710266cae8ce50daba93bbced65befce558e
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-21 11:03:22 -0500

    add contentful hero component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,4 +1,23 @@
+import heroImg from './assets/hero.svg';
+
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>contentful CMS</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+            quaerat unde ducimus nam accusamus totam odio fugit repellat tempora
+            enim quasi ex. Vel aspernatur, molestiae eveniet aut nulla pariatur!
+            Unde. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="hero image" className="img" />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Hero;
